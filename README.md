e8c9c67e649f01a7e901a0be184486526d32a1e7
Nanilabrute <komi.daniel.anani@student.ehb.be>
2021-11-07 22:19:32 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# crypto-NaniLabrute
+crypto-NaniLabrute created by GitHub Classroom
+
+follow these steps to run my code
+1. install npm packages with: npm install 
+2. run the code with: node main.js
---
+++ main.js
@@ -0,0 +1,54 @@
+const request = require('simple-get');
+const sodium = require('libsodium-wrappers');
+
+var url = 'https://79vo67ipp9.execute-api.eu-west-1.amazonaws.com/Prod/decrypt/challenges/'
+var kid, key, challenge, nonce, options;
+
+async function main(){
+    options = {
+        method: 'POST',
+         url: url,
+         json: true
+       }
+
+    // send a post request
+    await request.concat(options, function (err, res, data) {
+        if (err) throw err
+        kid = data.kid
+
+        // decode key, challenge and nonce
+        key = sodium.from_base64(data.key, sodium.base64_variants.ORIGINAL)
+        challenge = sodium.from_base64(data.challenge, sodium.base64_variants.ORIGINAL)
+        nonce = sodium.from_base64(data.nonce, sodium.base64_variants.ORIGINAL)
+
+        // decrypte the challenge
+        challenge = sodium.crypto_secretbox_open_easy(challenge, nonce, key);
+        // encode the challenge
+        challenge = sodium.to_base64(challenge, sodium.base64_variants.ORIGINAL);
+
+        // call the function hulp that make a delete request with the encoded challenge
+        // the reason is that we can't use the challenge variable out of this scope
+        hulp()
+    })
+}
+
+async function hulp(){
+
+    body = {'plaintext': challenge}
+
+    options = {
+        method: 'DELETE',
+        url: url + kid,
+        // it need to be a string 
+        body: JSON.stringify(body)
+       }
+
+    await request.delete(options, function (err, res) {
+        if (err) throw err
+        console.log(res.statusCode)
+        res.pipe(process.stdout)
+    })
+}
+
+// main
+main()
